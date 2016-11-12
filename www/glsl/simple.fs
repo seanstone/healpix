7eb7ec5f93e624b08b875ce1355a92302b2b5c73
En Shih <seanstone5923@gmail.com>
2016-11-13 00:57:22 +0800

    Convert HEALPIx code to GLSL

--- www/glsl/simple.fs
+++ www/glsl/simple.fs
@@ -3,13 +3,38 @@ uniform vec3 	iResolution;
 uniform float 	iGlobalTime;
 uniform vec4 	iMouse;
 
+#define M_PI 3.14159265358979
+#define M_PI_2 (M_PI/2.)
+#define M_PI_4 (M_PI/4.)
+
 //http://stackoverflow.com/questions/15057720/read-pixels-in-webgltexture-rendering-webgl-to-texture
 
 /* HEALPix */
 
 /* (tht, phi) --> (x,y) --> pixel */
 
+uniform int Dim;
 const int H = 4, K = 3;
+
+float facet_xwidth_2()   	{ return M_PI / float(H); }
+float facet_yheight_2()  	{ return M_PI / (float(K)+1.); }
+float facet_xwidth()     	{ return 2. * facet_xwidth_2(); }
+float facet_yheight()    	{ return 2. * facet_yheight_2(); }
+vec2 facet_xydim()     		{ return vec2(facet_xwidth(), facet_yheight()); }
+float x_c(int h, int k)  	{ return float(h) * facet_xwidth() + float(k) * facet_xwidth_2(); }
+float y_c(int k)         	{ return (float(k) - (float(K)-1.)/2.) * facet_yheight_2(); }
+vec2 uv_c(int f)
+{
+	int h = int(mod(float(f),float(H))), k = f/H;
+	float uc = - float(h) - (float(K)-1.)/4.;
+	float vc = - float(h) - float(k) + (float(K)-1.)/4.;
+	return vec2(uc, vc);
+}
+float y_x()              	{ return M_PI_2 * (float(K)-1.) / float(H); }
+float sigma(float y)     	{ return (float(K)+1.)/2. - abs(y*float(H))/M_PI; }
+
+float tht_x()              	{ return asin((float(K)-1.)/float(K)); }
+float sigma_t(float tht)	{ return sqrt(float(K) * (1.-abs(sin(tht)))); }
 
 struct Pixel
 {
@@ -19,13 +44,13 @@ struct Pixel
 
 Pixel P(vec2 xy)
 {
-	vec2 uv = mat2({-1, -1, 1, -1}) * (xy / facet_xydim());
-	vec2 hk = uv + vec2(0.5) + vec2((K-1)/4.0, -(K-1)/4.0);
+	vec2 uv = mat2(-1, -1, 1, -1) * (xy / facet_xydim());
+	vec2 hk = uv + vec2(.5) + vec2((float(K)-1.)/4., -(float(K)-1.)/4.);
 	vec2 hkc = floor(hk);
-	int h = -hkc.x;
-	int k = -hkc.y - h;
+	int h = int(-hkc.x);
+	int k = int(-hkc.y) - h;
 	int f = h + H * k;
-	vec2 ij = Dim * (hk - hkc);
+	vec2 ij = float(Dim) * (hk - hkc);
 	return Pixel(f, ij);
 }
 
@@ -35,20 +60,20 @@ vec2 XY(float tht, float phi)
 	if (abs(tht) < tht_x())
 	{
 		x = phi;
-		y = M_PI_2 * K / H * sin(tht);
+		y = M_PI_2 * float(K) / float(H) * sin(tht);
 	}
 	else
 	{
-		float phi_c = 0;
-		float tht_c = 0;
+		float phi_c = 0.;
+		float tht_c = 0.;
 		for(int h=-H; h<H; h++)
 		{
-			float c = h * 2 * M_PI / H ;
+			float c = float(h) * 2. * M_PI / float(H) ;
 			if (abs(c - phi) < abs(phi_c - phi)) phi_c = c;
 		}
 		for(int k=-K; k<K; k++)
 		{
-			float c = k * M_PI / (K+1) ;
+			float c = float(k) * M_PI / (float(K)+1.) ;
 			if (abs(c - tht) < abs(tht_c - tht)) tht_c = c;
 		}
 
@@ -56,34 +81,32 @@ vec2 XY(float tht, float phi)
 		if (x < -M_PI) x += M_PI;
 		if (x > M_PI) x -= M_PI;
 
-		y = (tht > 0 ? 1:-1) * M_PI / H * ( (K+1)/2.0 - sigma_t(tht) );
+		y = (tht > 0. ? 1.:-1.) * M_PI / float(H) * ( (float(K)+1.)/2. - sigma_t(tht) );
 	}
 	return vec2(x, y);
 }
 
 /* pixel --> (x,y) --> (tht,phi) */
 
-float2 XY(Pixel p)
+vec2 XY(Pixel p)
 {
-	//cout << "f:" << p.f << " ij:" << p.ij << "\t";
-	float2 ij = p.ij;
+	vec2 ij = p.ij;
 	//if (ij.x == 0) ij.x += 10-3;
 	//if (ij.y == 0) ij.y += 10-3;
 	//if (ij.x == Dim) ij.x -= 10-3;
 	//if (ij.y == Dim) ij.y -= 10-3;
-	float2 uv = uv_c(p.f) + (float2)ij/(float)Dim - float2(0.5);
-	float2 xy = facet_xydim() * (mat2({-1, 1, -1, -1}) / 2.0f * uv);
-	//cout << xy << "\t";
+	vec2 uv = uv_c(p.f) + ij/float(Dim) - vec2(.5);
+	vec2 xy = facet_xydim() * (mat2(-1, 1, -1, -1) / 2. * uv);
 	return xy;
 }
 
-float2 PhiSintht(float2 xy, int f)
+vec2 PhiSintht(vec2 xy, int f)
 {
-	int h = f%H, k = f/H;
+	int h = int(mod(float(f),float(H))), k = f/H;
 	float x = xy.x, y = xy.y;
-	float phi       = fabs(y) <= y_x() ?   x : x_c(h, k) + (x-x_c(h, k)) / sigma(y);
-	float sintht    = fabs(y) <= y_x() ?   y * H / M_PI_2 / K : (y > 0 ? 1:-1) * ( 1 - sigma(y)*sigma(y)/K );
-	return float2(phi, sintht);
+	float phi       = abs(y) <= y_x() ?   x : x_c(h, k) + (x-x_c(h, k)) / sigma(y);
+	float sintht    = abs(y) <= y_x() ?   y * float(H) / M_PI_2 / float(K) : (y > 0. ? 1.:-1.) * ( 1. - sigma(y)*sigma(y)/float(K) );
+	return vec2(phi, sintht);
 }
 
 /* */
