1ceadcd4a682a7b6a573e89dccd20d07e27fb84c
En Shih <seanstone5923@gmail.com>
2016-11-12 23:48:43 +0800

    Colored sphere

--- www/glsl/simple.fs
+++ www/glsl/simple.fs
@@ -3,11 +3,14 @@ uniform vec3 	iResolution;
 uniform float 	iGlobalTime;
 uniform vec4 	iMouse;
 
+//http://stackoverflow.com/questions/15057720/read-pixels-in-webgltexture-rendering-webgl-to-texture
+
 struct Sphere
 {
 	vec3 origin;
 	float radius;
 };
+// TODO: make sphere uniform
 
 struct Camera
 {
@@ -27,6 +30,12 @@ struct Ray
 	vec3 dir;
 };
 
+struct Intersection
+{
+	bool intersect;
+	vec3 point;
+};
+
 Ray createCameraRay(Camera camera, vec2 uv)
 {
 	vec3 rayDir = (1. - 2. * uv.x) * tan(camera.fov * .5) * camera.right * camera.aspectRatio
@@ -36,22 +45,24 @@ Ray createCameraRay(Camera camera, vec2 uv)
 	return Ray(camera.position, rayDir);
 }
 
-bool findSphereIntersection(Sphere sphere, Ray ray)
-{
+Intersection findSphereIntersection(Sphere sphere, Ray ray)
+{	/* https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection */
 	vec3 c = sphere.origin;
 	float r = sphere.radius;
 	vec3 l = ray.dir;
 	vec3 o = ray.origin;
 
-	float a = - dot(o-c,l) / dot(l,l);
-	float d = sqrt(dot(o-c,o-c) + 2.*a*dot(o-c,l) + a*a*dot(l,l));
-	return d < r;
+	float D = dot(l,c-o) * dot(l,c-o) - dot(c-o,c-o) + r*r;
+	if (D < 0.) return Intersection(false, vec3(0));
+	float a = dot(l,c-o) + sqrt(D);
+	if (a < 0.) return Intersection(false, vec3(0));
+	return Intersection(true, o + a*l);
 }
 
 void main()
 {
 	Camera camera;
-	camera.position = vec3(0,0,2);
+	camera.position = vec3(0,0,-4);
 	camera.resolution = iResolution.xy;
 	camera.fov = radians(90.);
 	camera.right = vec3(1,0,0);
@@ -64,6 +75,9 @@ void main()
 	sphere.radius = 3.;
 
 	Ray ray = createCameraRay(camera, gl_FragCoord.xy / iResolution.xy);
-	float r = findSphereIntersection(sphere, ray) ? 1. : 0.;
-	gl_FragColor = vec4(r, r, r, 1);
+	Intersection intersection = findSphereIntersection(sphere, ray);
+	float r = sphere.radius;
+	vec3 p = intersection.point/r * .5 + 1.;
+	if (!intersection.intersect) p = vec3(0,0,0);
+	gl_FragColor = vec4(p * 0.5, 1);
 }
